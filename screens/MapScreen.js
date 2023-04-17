ea4d40c7a09471dcdf162d41aed589a402ba29cc
ImJkino <aquino.joseph94@gmail.com>
2023-04-17 09:20:22 -0700

    first commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>It is the map screen you're looking for.</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({});
---
+++ slices/navSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payoad;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payoad;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payoad;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+//This is information for the app, where are you for pick up, wheres the destination, and the time for travel
+//preparatoion for the information that will be used in the app
+//actions: we need to use them
+//pushing information into data layer
+
+//Selectors:
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
